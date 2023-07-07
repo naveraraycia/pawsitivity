b8da74fc158cc6c2ce5ee17c255e02c54156875e
naveraraycia <naveraraycia@yahoo.com>
2023-07-07 15:10:29 -0700

    Changed animation on Branding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <div className=" py-6 px-12 md:px-40 md:py-10">
       <nav>
-        <h4 data-aos="fade-right" data-aos-delay="650" className="uppercase font-sans font-bold text-slate text-2xl self-center"><span className="text-sky-darker">paw</span>sitivity</h4>
+        <h4 data-aos="fade-down" data-aos-delay="650" className="uppercase font-sans font-bold text-slate text-2xl self-center"><span className="text-sky-darker">paw</span>sitivity</h4>
       </nav>
     </div>
   )
