e6dd154c394b55fb03feebd35e2311d909905002
naveraraycia <naveraraycia@yahoo.com>
2023-07-06 22:15:58 -0700

    added language btns in the UI as well as hover effects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({children, shadow, height}) {
   return (
-    <div id="card" className={`py-5 px-10 text-xs text-slate text-left bg-white text-light font-sans tracking-widest leading-6 max-w-2xl ${shadow === 'blue' && 'blue-box'} xl:text-sm xl:px-12 xl:py-8 xl:leading-7 ${height === 'full'&& 'h-full'}`}>
+    <div id="card" className={`py-5 px-10 text-xs text-slate duration-200 text-left bg-white text-light font-sans tracking-widest leading-6 max-w-2xl ${shadow === 'blue' && 'blue-box'} xl:text-sm xl:px-12 xl:py-8 xl:leading-7 ${height === 'full'&& 'h-full'}`}>
       {children}
     </div>
   )
--- src/components/FixedLanguageBtn.jsx
+++ src/components/FixedLanguageBtn.jsx
@@ -1,6 +1,6 @@
 function FixedLanguageBtn() {
   return (
-    <div className="fixed bottom-10 right-16">
+    <div className="fixed bottom-10 right-16 z-50">
       <div className="flex space-x-5">
         <button className="rounded-3xl bg-white text-slate font-bold tracking-wider font-sans uppercase py-3 px-5 lang-btn duration-200">eng</button>
         <button className="rounded-3xl bg-white text-slate font-bold tracking-wider font-sans uppercase py-3 px-5 lang-btn duration-200">fil</button>
